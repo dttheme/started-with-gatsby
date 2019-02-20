5099bc8ae5ac1772cd8803875dbb587132168583
Dyanna Turner <dttheme@gmail.com>
2019-02-19 23:19:44 -0500

    Initial commit

---
+++ src/pages/about.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Link } from "gatsby";
+import Layout from "../components/layout";
+
+import styles from "./about.module.css";
+
+const User = props => (
+  <div className={styles.user}>
+    <img src={props.avatar} className={styles.avatar} alt="" />
+    <div className={styles.description}>
+      <h2 className={styles.username}>{props.username}</h2>
+      <p className={styles.excerpt}>{props.excerpt}</p>
+    </div>
+  </div>
+);
+
+export default () => (
+  <Layout>
+    About!
+    <Link to="/">Home</Link>
+    <User
+      username="Jane Doe"
+      avatar="https://s3.amazonaws.com/uifaces/faces/twitter/adellecharles/128.jpg"
+      excerpt="I'm Jane Doe. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+    />
+    <User
+      username="Bob Smith"
+      avatar="https://s3.amazonaws.com/uifaces/faces/twitter/vladarbatov/128.jpg"
+      excerpt="I'm Bob Smith, a vertically aligned type of guy. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+    />
+  </Layout>
+);
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,15 @@
-import React from "react"
+import React from "react";
+import { Link } from "gatsby";
+import Layout from "../components/layout";
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <Layout>
+    Hello world!
+    <Link to="/about">About</Link>
+    <h1>Hi! I'm building a fake Gatsby site as part of a tutorial!</h1>
+    <p>
+      What do I like to do? Lots of course but definitely enjoy building
+      websites.
+    </p>
+  </Layout>
+);
